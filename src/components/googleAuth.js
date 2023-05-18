8aa99e56b9ef9211824182a73900eecfa74b4ad4
AKILAN-SIVA <akilanlakshmi2003@gmail.com>
2023-05-18 19:09:42 +0530

    first commit

--- src/components/googleAuth.js
+++ src/components/googleAuth.js
@@ -9,7 +9,7 @@ export default function GoogleAuth() {
     }
     return(
         <div className='bg-Bg bg-cover flex justify-center pt-80 pb-96 '>
-            <GoogleOAuthProvider clientId='593844356138-j8fv8s4red4dncvhc9f5esfjagadf2ml.apps.googleusercontent.com'>
+            <GoogleOAuthProvider clientId={clientId}>
                 <GoogleLogin onSuccess={signInSuccess} onError={signInError} shape='circle' width='12' />
             </GoogleOAuthProvider>
         </div>
